d5608315f6b12180b8dc9849a39e67ce89c618f2
Russ Taylor <russ@reliasolve.com>
2023-07-28 10:42:27 -0400

    Switching to using the Phenix CCTBX build

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source ~/rlab/cctbx/build/setpaths.sh
-export MMTBX_CCP4_MONOMER_LIB=/home/taylorr/rlab/phenix/modules/chem_data/geostd
-export CLIBD_MON=/home/taylorr/rlab/phenix/modules/chem_data/mon_lib
+source ~/rlab/phenix/build/setpaths.sh
+#source ~/rlab/cctbx/build/setpaths.sh
+#export MMTBX_CCP4_MONOMER_LIB=/home/taylorr/rlab/phenix/modules/chem_data/geostd
+#export CLIBD_MON=/home/taylorr/rlab/phenix/modules/chem_data/mon_lib
